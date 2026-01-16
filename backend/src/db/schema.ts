79d3faf960f93004bc9cc7de86c93740010d0a5a
nourhene <benothmennourhen8@gmail.com>
2026-01-16 21:13:08 +0100

    feat: setup database connection in src/db

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, PgTable,text,timestamp,uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+export const users=pgTable("users",{
+
+})
+export const products=pgTable("products",{
+
+})
+export const comments=pgTable("comments",{
+    
+})
